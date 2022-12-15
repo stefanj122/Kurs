6dc80992912ea9a6bef1d10096e2181e593bc968
stefanj122 <stefan.jeftic122@gmail.com>
2022-12-15 17:11:29 +0100

    Week7 changes

--- Week7/zad1v2.js
+++ Week7/zad1v2.js
@@ -1,9 +1,18 @@
 import axios from "axios";
 import fs from "fs";
-
-const response = await axios.get("https://jsonplaceholder.typicode.com/posts", {
-  headers: { "Accept-Encoding": "gzip,deflate,compress" },
-});
-response.data.forEach((element) => {
-  fs.appendFileSync("posts.txt", JSON.stringify(element) + "\n");
-});
+async function axiosTest() {
+  try {
+    const response = await axios.get(
+      "https://jsonplaceholder.typicode.com/posts",
+      {
+        headers: { "Accept-Encoding": "gzip,deflate,compress" },
+      }
+    );
+    response.data.forEach((element) => {
+      fs.appendFileSync("posts.txt", JSON.stringify(element) + "\n");
+    });
+  } catch (e) {
+    console.log(e);
+  }
+}
+axiosTest();
